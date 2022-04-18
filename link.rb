2f8a997f56153b8b6388b4a9d0d1469db5466fc2
Eliminate505 <zvm_fbi@mail.ru>
2022-04-18 13:21:21 +0500

    post class ancetors

---
+++ link.rb
@@ -0,0 +1,20 @@
+class Link < Post
+  def initialize
+    super
+    @url = ""
+
+
+  end
+
+  def read_from_conole
+
+  end
+
+  def to_strings
+
+
+  end
+
+
+
+end
---
+++ memo.rb
@@ -0,0 +1,13 @@
+class Memo < Post
+  def read_from_conole
+
+  end
+
+  def to_strings
+
+  end
+
+  end
+
+
+end
