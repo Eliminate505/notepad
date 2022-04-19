c95b13d2317d03ba39aa7a41be6b14c367f8d5e2
Eliminate505 <zvm_fbi@mail.ru>
2022-04-19 18:53:36 +0500

    Notepad v.1, working)

--- post.rb
+++ post.rb
@@ -1,4 +1,12 @@
 class Post
+  def self.post_types
+    [Memo,Link,Task]
+
+  end
+  def self.create(type_index)
+    return post_types[type_index].new
+
+  end
 
   def initialize
     @created_at = Time.now
@@ -14,10 +22,10 @@ class Post
   end
 
   def save
-    file = File.new(file_path, "w:UTF-8" )
-    for item in  to_strings do
-      puts file(item)
-    end
+    file = File.new(file_path, 'w:UTF-8') # открываем файл на запись
+
+    to_strings.each { |string| file.puts(string) }
+
     file.close
   end
   def file_path
